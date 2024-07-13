344bc9d39dae9e30bfcdb365c984f554b6de4c67
Tatyana Stasenko <sale_rim@mail.ru>
2024-07-13 17:18:39 +0700

    save file

--- FamilyTreeIterator.java
+++ FamilyTreeIterator.java
@@ -1,21 +1,22 @@
-// import java.util.Iterator;
-// import java.util.List;
+import java.util.Iterator;
+import java.util.List;
 
-// public class FamilyTreeIterator implements Iterator<Human> {
-//     private List<Human> list;
-//     private int index;
 
-//     public FamilyTreeIterator(List<Human> list){
-//         this.list = list;
-//         index = 0;
-//     }
+public class FamilyTreeIterator implements Iterator<Human> {
+    private List<Human> list;
+    private int index;
 
-//     @Override
-//     public boolean hasNext() {
-//         return index< list.size();
-//     }
-//     @Override
-//     public Human next() {
-//         return list.get(index++);
-//     }
-// }
+    public FamilyTreeIterator(List<Human> list){
+        this.list = list;
+        index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index< list.size();
+    }
+    @Override
+    public Human next() {
+        return list.get(index++);
+    }
+}
