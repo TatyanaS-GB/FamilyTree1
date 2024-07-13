f160b7b4e87e5c31e7820d6d6ca845294365f37c
Tatyana Stasenko <sale_rim@mail.ru>
2024-07-13 17:19:46 +0700

    save file

--- HumanComparatorByName.java
+++ HumanComparatorByName.java
@@ -1,9 +1,9 @@
-// import java.util.Comparator;
+import java.util.Comparator;
 
-// public class HumanComparatorByName implements Comparator<Human> {
+public class HumanComparatorByName implements Comparator<Human> {
 
-//     @Override
-//     public int compare(Human o1, Human o2) {
-//         return o1.getName().compareTo(o2.getName());
-//     }
-// }
+    @Override
+    public int compare(Human o1, Human o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
